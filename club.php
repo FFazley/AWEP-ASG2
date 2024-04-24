426c6779256b7a209ad725702b0d5ae699b1004e
Fazley <22FTT1494@student.pb.edu.bn>
2024-04-24 08:16:29 +0800

    Update club.php with new images and links, and fix search functionality

--- club.php
+++ club.php
@@ -1,3 +1,35 @@
+<?php
+require 'database.php';
+
+// Initialize the list of club items
+$clubItems = array(
+    array("name" => "badminton", "image" => "img/badminton1.png"),
+    array("name" => "rugby", "image" => "img/rugby1.png"),
+    array("name" => "judo", "image" => "img/judo.png")
+);
+
+// Check if the search query is set
+if(isset($_GET['search'])) {
+    // Get the search query from the URL
+    $search = $_GET['search'];
+
+    // Filter club items based on the search query
+    $filteredItems = array_filter($clubItems, function($item) use ($search) {
+        return stripos($item['name'], $search) !== false;
+    });
+
+    // Update clubItems to contain only filtered items
+    $clubItems = $filteredItems;
+  
+    // Check if any items match the search query
+    if(empty($clubItems)) {
+        echo "<p class='text-white text-center text-2xl' style='position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'>Search not available</p>";
+    }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,49 +86,49 @@
             }
         
     </style>
+
+
 </head>
 <body class="flex flex-col" style="background-color: #08415C;">
+   
     <div class="sm:w-1/2 md:w-2/3 lg:w-3/4 mx-auto">
     <img src="img/pb.jpeg" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1; opacity: 0.10;">
     <br>
     <div style="position: absolute; top: 10px; left: 10px;">
-    <a href="home.php" class="text-white text-lg" ><i class="fas fa-arrow-left text-black"></i> </a>
+    <a href="club.php" class="text-white text-lg" ><i class="fas fa-arrow-left text-black"></i> </a>
     <br>
     </div>
 <h3 class="text-2xl font-bold mb-4 text-white text-center">Club</h3>
-<br>
-    <div class="flex justify-center">
+    <br>
+    
+    
+
+    <!-- Search Form -->
+    <form action="club.php" method="GET" class="flex justify-center mb-20">
         <div class="relative">
-            <input type="text" class="border border-gray-300 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:border-transparent w-80" placeholder="Search">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <i class="fas fa-search text-gray-400"></i>
+            <input type="text" name="search" placeholder="Search" class="border border-gray-300 rounded-md py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:border-transparent w-80">
+            <i class="fas fa-search text-gray-400 absolute top-3 left-3"></i>
+        </div>
+        <button type="submit"></button>
+    </form>
+
+    <!-- Display Club Items -->
+    <div class="flex justify-center flex-wrap">
+        <?php foreach ($clubItems as $item): ?>
+            <div class="flex justify-center">
+                <a href="<?php echo $item['name'] . '.php'; ?>">
+                    <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>" class="w-25 h-45" style="border-radius: 20px;">
+                </a>
             </div>
-        </div>
+            <br>
+        <?php endforeach; ?>
     </div>
-    <br>
-    <br>
-        <div class="flex justify-center">
-            <a href="badminton.php">
-            <img src="img/badminton1.png" alt="Badminton" class="w-25 h-45" style="border-radius: 20px;">
-            </a>
-        </div>
-    <br>
-    <br>
-        <div class="flex justify-center">
-        <a href="rugby.php">
-            <img src="img/rugby1.png" alt="Rugby" class="w-25 h-45" style="border-radius: 50px;">
-        </a>
-        </div>
-    <br>
-    <br>
-        <div class="flex justify-center">
-        <a href="judo.php">
-            <img src="img/judo.png" alt="judo" class="w-30 h-30" style="border-radius: 20px;">
-        </a>
-        </div>
+
+</body>
+</html>
+
         <br>
         <br>
-       
     <nav class="bg-yellow-300 text-white px-4 py-2 flex justify-center" style="border-top-left-radius: 40%; border-top-right-radius: 40%; border: 1px solid black;position: fixed; bottom: -10px; left: -10px; right: -10px;">
         <ul class="flex space-x-10 items-center"> <!-- Increased space-x value to 8 -->
             <li class="nav-item"><a href="home.php"><i class="fas fa-home" style="color: black; font-size: 24px;"></i></a></li>
@@ -105,6 +137,6 @@
             <li class="nav-item"><a href="UserProfile.php"><i class="fas fa-user" style="color: black; font-size: 24px;"></i></a></li>
         </ul>
     </nav>
-    </div>
+
         </body>
 </html>
